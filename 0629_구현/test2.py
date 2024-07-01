479b22fd522e761e63032a468f7dbe21e08f2279
임보정 <126267395+imbojeong@users.noreply.github.com>
2024-07-01 16:55:23 +0900

    0629 coding

---
+++ 0629_구현/test2.py
@@ -0,0 +1,35 @@
+score = int(input())
+
+if 90 <= score <= 100:
+    print("A")
+    
+elif 80 <= score <= 89:
+    print("B")
+    
+elif 70 <= score <= 79:
+    print("C")
+elif 60 <= score <= 69:
+    print("D")
+else:
+    print("F")
+    
+# 입력을 받고 정수 리스트로 변환
+# scores = list(map(int, input().split()))
+# score = map(int, input().split())
+# print(scores)
+# print(score)
+
+# # 리스트의 각 점수를 반복
+# for score in scores:
+#     if 90 <= score <= 100:
+#         print("A")
+#     elif 80 <= score <= 89:
+#         print("B")
+#     elif 70 <= score <= 79:
+#         print("C")
+#     elif 60 <= score <= 69:
+#         print("D")
+#     else:
+#         print("F")
+
+
